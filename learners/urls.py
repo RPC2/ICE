2d8f11b9ace3d9ac0ae572a9b1f55546a89bd744
Cynthia Chen <RPC2@users.noreply.github.com>
2019-03-25 07:52:08 +0800

    finish quiz

--- learners/urls.py
+++ learners/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # TODO: implement this
     # path('<str:username>/view_quiz/', views.ViewQuiz.as_view(), name='viewQuiz'),
     # path('<str:username>/take_quiz/', views.take_quiz, name='takeQuiz'),
-    path('view_quiz/', views.view_quiz, name='view_quiz'),
+
     path('take_quiz/', views.take_quiz, name='take_quiz'),
     path('view_result/', views.view_result, name='view_result'),
 
--- learners/views.py
+++ learners/views.py
@@ -1,46 +1,39 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.views.decorators.csrf import csrf_protect
 from django.urls import reverse
 from django.views import generic
 
+
 from .models import *
 from courses.models import *
+from courses.forms import *
 from learners.models import *
 
 
-def view_quiz(request):
-    module = get_object_or_404(Module, title__startswith="Chapter 1")
-    question_list = list(module.quizquestion_set.all())
-
-    choice_list = []
-    for i in range(len(question_list)):
-        choice_list.append(list(question_list[i].quizchoice_set.all()))
-
-    return render(request, 'take_quiz.html', {'q_list': question_list, 'num_of_question': range(len(question_list)),
-                                              'c_list': choice_list})
-
-
+@csrf_protect
 def take_quiz(request):
     module = get_object_or_404(Module, title__startswith="Chapter 1")
     question_list = list(module.quizquestion_set.all())
 
-    print("Hi there")
+    if request.method == 'POST':
+        form = QuizForm(request.POST or None, questions=question_list)
+        total = 0
+        if form.is_valid():
+            for (question_description, answer) in form.answers():
+                choice = QuizChoice.objects.get(choice_text=answer)
+                total += choice.value
+                QuizResult.objects.create(total_score=total)
+            return HttpResponseRedirect('/learner/view_result/')
+    else:
+        form = QuizForm(questions=question_list)
 
-    total = 0
-
-    for q in question_list:
-        selected_choice = q.quizchoice_set.get(pk=request.POST['choice'])
-        score = selected_choice.value
-        total += score
-
-    quiz_result = QuizResult.objects.create(total_score = total)
-    print(total)
-    return HttpResponseRedirect('learners:view_result')
+    return render(request, 'take_quiz.html', {'form': form})
 
 
 def view_result(request):
     latest_submission = QuizResult.objects.get(pk=len(list(QuizResult.objects.all())))
-    if latest_submission.total_score > 10:
+    if latest_submission.total_score >= 10:
         result = "passed"
     else:
         result = 'failed'
