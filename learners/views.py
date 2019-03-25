49ea7a104af40f9e2a50d7daaa13ec46237451eb
Cynthia Chen <RPC2@users.noreply.github.com>
2019-03-25 18:50:36 +0800

    learner part

--- learners/views.py
+++ learners/views.py
@@ -25,3 +25,32 @@ def module_detail(request, moduleid):
     components = Component.objects.filter(Module_id=module.id)
     return render(request, 'learner_module_detail.html', {'components': components, 'module': module})
 
+@csrf_protect
+def take_quiz(request):
+    module = get_object_or_404(Module, title__startswith="Chapter 1")
+    question_list = list(module.quizquestion_set.filter(selected=True))
+
+    if request.method == 'POST':
+        form = QuizForm(request.POST or None, questions=question_list)
+        total = 0
+        if form.is_valid():
+            for (question_description, answer) in form.answers():
+                choice = QuizChoice.objects.get(choice_text=answer)
+                total += choice.value
+                QuizResult.objects.create(total_score=total)
+            return HttpResponseRedirect('/learner/view_result/')
+    else:
+        form = QuizForm(questions=question_list)
+
+    return render(request, 'take_quiz.html', {'form': form})
+
+
+def view_result(request):
+    latest_submission = QuizResult.objects.get(pk=len(list(QuizResult.objects.all())))
+    if latest_submission.total_score >= 10:
+        result = "passed"
+    else:
+        result = 'failed'
+    return render(request, 'quiz_result.html', {'total_score': latest_submission.total_score,
+                                                'pass_or_fail': result})
+
