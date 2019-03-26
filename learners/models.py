175c4f356820ab6854d1687350dc453c47b2682c
Renjie PI <pipilu@connect.hku.hk>
2019-03-26 16:52:11 +0800

    in order

--- learners/models.py
+++ learners/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-
 # Create your models here.
-
 
 class Learner(models.Model):
     username = models.CharField(max_length=100)
@@ -11,6 +9,8 @@ class Learner(models.Model):
     last_name = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     last_quiz_result = models.IntegerField(default=0)
+    def __str__(self):
+        return self.username
 
 
 class QuizResult(models.Model):
