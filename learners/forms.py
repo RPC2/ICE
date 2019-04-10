974de8e1d15ff33d81131e3e2eab2b28dd93e56a
王涵彦 <why@wanghanyandeMacBook-Pro.local>
2019-04-10 16:09:42 +0800

    commit

--- learners/forms.py
+++ learners/forms.py
@@ -6,6 +6,9 @@ from django.forms.utils import ValidationError
 
 class SendEmailForm(forms.Form):
     staff_id= forms.CharField(max_length=30,help_text='Required. Inform your staff id.')
+class SignupForm(forms.Form):
+    username= forms.CharField(max_length=30,help_text='Required. Inform your username.')
+    password = forms.CharField(max_length=30, help_text='Required. Inform your password.')
 # class StudentSignUpForm(UserCreationForm):
 #     staff_id = forms.CharField(max_length=100, help_text='Required. Inform your staff id.')
 #     class Meta(UserCreationForm.Meta):
